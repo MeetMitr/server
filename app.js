b076b9a567e17a02a232a88576dd99b7bd69c2dd
Thunthup Kiratisehwe <thunthup123@gmail.com>
2021-11-19 15:57:16 +0700

    add test query

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-const express = require('express')
-const mysql = require('mysql')
-const app = express()
-const cors =require('cors')
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    user: "root",
-    host: "localhost",
-    password: "admin",
-    database: "test"
-})
+  user: "root",
+  host: "localhost",
+  password: "admin",
+  database: "test",
+});
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Ahoy!' })
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Ahoy!" });
+});
+
+app.get("/test", (req, res) => {
+  db.query("select * from name", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, (req, res) => {
-    console.log(`Server Started at PORT ${PORT}`);
+  console.log(`Server Started at PORT ${PORT}`);
 });
-
-
