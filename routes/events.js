eef7679918b97c672b1d777bb3c38e05f3e2981b
GNIFING <narawich.fing@gmail.com>
2021-11-22 19:40:32 +0700

    event detail api

--- routes/events.js
+++ routes/events.js
@@ -11,44 +11,56 @@ const db= mysql.createConnection({
   database: "main",
 });
 
+// router.get("/", (req, res) => {
+//   res.json({
+//     "00001": {
+//       name: "Poom's Party",
+//       datetime: "2001-03-05 01:01:01",
+//       location: "Address1",
+//     },
+//     "00002": {
+//       name: "ม็อบสนับสนุนสถาบัน",
+//       datetime: "2001-03-05 02:02:02",
+//       location: "Address2",
+//     },
+//     "00003": {
+//       name: "ม็อบสนับสนุนสถาบัน2",
+//       datetime: "2001-03-05 02:02:02",
+//       location: "Address2",
+//     },
+//     "00004": {
+//       name: "ม็อบสนับสนุนสถาบัน3",
+//       datetime: "2001-03-05 03:03:03",
+//       location: "Address3",
+//     },
+//     "00005": {
+//       name: "ม็อบสนับสนุนสถาบัน4",
+//       datetime: "2001-03-05 04:04:04",
+//       location: "Address4",
+//     },
+//     "00006": {
+//       name: "ม็อบสนับสนุนสถาบัน5",
+//       datetime: "2001-03-05 05:05:05",
+//       location: "Address5",
+//     },
+//     "00007": {
+//       name: "ม็อบสนับสนุนสถาบัน6",
+//       datetime: "2001-03-05 06:06:06",
+//       location: "Address6",
+//     },
+//   });
+// });
+
 router.get("/", (req, res) => {
-  res.json({
-    "00001": {
-      name: "Poom's Party",
-      datetime: "2001-03-05 01:01:01",
-      location: "Address1",
-    },
-    "00002": {
-      name: "ม็อบสนับสนุนสถาบัน",
-      datetime: "2001-03-05 02:02:02",
-      location: "Address2",
-    },
-    "00003": {
-      name: "ม็อบสนับสนุนสถาบัน2",
-      datetime: "2001-03-05 02:02:02",
-      location: "Address2",
-    },
-    "00004": {
-      name: "ม็อบสนับสนุนสถาบัน3",
-      datetime: "2001-03-05 03:03:03",
-      location: "Address3",
-    },
-    "00005": {
-      name: "ม็อบสนับสนุนสถาบัน4",
-      datetime: "2001-03-05 04:04:04",
-      location: "Address4",
-    },
-    "00006": {
-      name: "ม็อบสนับสนุนสถาบัน5",
-      datetime: "2001-03-05 05:05:05",
-      location: "Address5",
-    },
-    "00007": {
-      name: "ม็อบสนับสนุนสถาบัน6",
-      datetime: "2001-03-05 06:06:06",
-      location: "Address6",
-    },
+  console.log("sending...")
+  let sql = 'SELECT eventId, name, takePlace, address, district, province, zipcode, imageURL FROM Event';
+  db.query(sql, (err, result) => {
+      if (err) throw err;
+    console.log(result);
+    res.json( result );
+
   });
+  // res.json({ message: "Ahoy!", id: id });
 });
 
 router.get("/:searchname", (req, res) => {
