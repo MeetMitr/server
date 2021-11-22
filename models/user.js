1cfa9c13a19028ef967206201fdeeb880be42cfa
supmine <supanart2010@hotmail.com>
2021-11-22 23:12:23 +0700

    mongo schema

--- models/user.js
+++ models/user.js
@@ -3,13 +3,64 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
+    userId: {
         type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    profileName: {
+        type: String,
+        required: true
+    },
+    birthdate: {
+        type: Date
+    },
+    displayPicURL: {
+        type: Array,
         required: true
     },
     password: {
         type: String,
         required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    middleName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    hideGender: {
+        type: String,
+        required: true
+    },
+    numberOfPenalty: {
+        type: Number,
+        required: true
+    },
+    bio: {
+        type: String,
+    },
+    phoneNo: {
+        type: String,
+    },
+    friends: {
+        type: Array
+    },
+    personalities: {
+        type: Array
     }
 });
 
