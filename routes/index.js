61fc1138ef799fdda94af6b3554291bfbfe2f9d7
d <assd@gmail.com>
2021-11-22 20:21:17 +0700

    send user value

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,11 @@ const db = mysql.createConnection({
 
 router.post('/' , (req, res) => {
     const {email, password} = req.body;
-    db.query("SELECT email,password FROM User WHERE email = " + "'" +email+"'", (err, rows, fields) => {
+    db.query("SELECT * FROM User WHERE email = " + "'" +email+"'", (err, rows, fields) => {
         if (!err){
             hash_password = rows[0].password;
             if (bcrypt.compareSync(password, hash_password)) {
-              res.json("login success");
+              res.json(rows[0]);
             }
             else {
                 res.json("login fail");
