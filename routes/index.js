d9c8924f6bd618de4b1fd4fb392713c4f54da3c8
d <assd@gmail.com>
2021-11-22 19:35:53 +0700

    disconnect connection

--- routes/index.js
+++ routes/index.js
@@ -9,48 +9,21 @@ const db = mysql.createConnection({
     database: "main",
 });
 
-
-// router.get("/add" , (req,res) => {
-//     var a = req.query.x
-//     var b = req.query.y
-//     res.send({"ans" : a, "ans2" : b})
-// })
-
-// console.log("hello");
-router.get("/", (req, res) => {
-    // console.log("hello");
-    // const {email, password} = req.body;
-    // let sql = "SELECT email,password FROM User WHERE email = " + "'" +email+"'";
-    // db.query(sql, (err, result) => {
-    //     if (err) throw err;
-    //   console.log(result);
-    //   res.json( result );
-    // });
-    db.connect(function(err) {
-        if (err) throw err;
-        const {email, password} = req.body;
-        var hash_password = "";
-        var sql = "SELECT email,password FROM User WHERE email = " + "'" +email+"'";
-        db.query(sql, function (err, result) {
-          if (err) throw err;
-          hash_password = result[0].password;
-          if (bcrypt.compareSync(password, hash_password)) {
-            res.json("login success");
-            console.log("succ");
-          }
-          else {
-              res.json("login fail");
-              console.log("fail");
-          }
-        });
-      });
-
-    //res.json({ message: "Ahoy!" });
-    // if(req.query.admin === 'true'){
-    //     next()
-    // } else {
-    //     res.send('No auth')
-    // }
-});
+router.post('/' , (req, res) => {
+    const {email, password} = req.body;
+    db.query("SELECT email,password FROM User WHERE email = " + "'" +email+"'", (err, rows, fields) => {
+        if (!err){
+            hash_password = rows[0].password;
+            if (bcrypt.compareSync(password, hash_password)) {
+              res.json("login success");
+            }
+            else {
+                res.json("login fail");
+            }
+        }
+        else
+            console.log(err);
+    })
+} );
 
 module.exports = router;
